2b90fea38e6385e4766b66cdd074cee85ea0c18d
venture7-soni <161804642+venture7-soni@users.noreply.github.com>
2024-03-01 12:53:12 +0530

    Create teamcity.sh

---
+++ teamcity.sh
@@ -0,0 +1,25 @@
+ 1  clear
+    2  yum update -y
+    3  clear
+    4  sudo yum update -y
+    5  sudo yum install java-1.8.0-openjdk-devel -y
+    6  wget https://download.jetbrains.com/teamcity/TeamCity-2022.10.1.tar.gz
+    7  wget https://download.jetbrains.com/teamcity/TeamCity-<version>.tar.gz
+    8  wget https://download.jetbrains.com/teamcity/TeamCity-2022.10.1.tar.gz
+    9  wget https://download.jetbrains.com/teamcity/TeamCity-2022.10.1.tar.gzwget https://download.jetbrains.com/teamcity/TeamCity-2022.10.1.tar.gz
+   10  wget https://download.jetbrains.com/teamcity/TeamCity-2022.10.1.tar.gz
+   11  ls
+   12  rm -rf *
+   13  wget https://download.jetbrains.com/teamcity/TeamCity-2022.10.1.tar.gz
+   14  ls
+   15  tar -xzf TeamCity-<version>.tar.gz
+   16  tar -xzf TeamCity-2022.10.1tar.gz
+   17  tar -xzf TeamCity-2022.10.1.tar.gz
+   18  ls
+   19  sudo mv TeamCity /opt
+   20  cd /opt/TeamCity
+   21  ls
+   22  sudo bin/runAll.sh start
+   23  l
+   24  ls
+   25  history
